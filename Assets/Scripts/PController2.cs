d1b3c165726ab20008d0ca59fb6b5352d676199a
Mohith Bhargav Donthu <dbhargav@quinnipiac.edu>
2023-05-10 00:45:10 -0400

    updates

---
+++ Assets/Scripts/PController2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PController2 : MonoBehaviour
+{
+    private List<int> inputValues = new List<int>();
+    private int[] code = { 1, 2, 4, 3 };
+
+    [SerializeField] private GameObject objectToDisable;
+
+    public void ButtonClicked(int buttonNumber)
+    {
+        inputValues.Add(buttonNumber);
+
+        if (inputValues.Count == code.Length && CheckCode())
+        {
+            objectToDisable.SetActive(false);
+        }
+    }
+
+    private bool CheckCode()
+    {
+        for (int i = 0; i < code.Length; i++)
+        {
+            if (inputValues[i] != code[i])
+            {
+                inputValues.Clear();
+                return false;
+            }
+        }
+
+        inputValues.Clear();
+        return true;
+    }
+}
